abb99971d075c6800714abeb0d74332e272823b3
Triburch <eeR_muuLa_97@hotmail.com>
2018-05-02 12:21:52 +0200

    Add Module Subtraction and Refactor Calculator
    Añadido el Modulo de resta y se ha refactorizado la clase Calculator para adaptarse a la peticion del desarrollo, ahora la clase envia la expresion al modulo de calculo que le toca y guarda el resultado en la propiedad result.

--- Calculator/src/ies/badia/calculator/main/Calculator.java
+++ Calculator/src/ies/badia/calculator/main/Calculator.java
@@ -17,6 +17,7 @@ public class Calculator {
 	private String op1;
 	private String operador;
 	private String op2;
+	private int result;
 	
 	
 	/**
@@ -54,6 +55,30 @@ public class Calculator {
 		
 		
 	}
+	
+	
+	/**
+	 * 
+	 * Opera la expresion, pasandosela al modulo aritmetico correcto,
+	 * Guarda el resultado en la propiedad this.result
+	 */
+	public void operate() {
+		switch (this.operador) {
+		case "+":
+			this.setResult(Addition.result(op1, op2));
+			break;
+		case "-":
+			this.setResult(Subtraction.result(op1, op2));
+			break;
+			
+			
+		
+		default:
+			
+			break;
+		}
+		
+	}
 
 
 	public String getOp1() {
@@ -84,6 +109,19 @@ public class Calculator {
 	public void setOp2(String op2) {
 		this.op2 = op2;
 	}
+
+
+
+	public int getResult() {
+		return result;
+	}
+
+
+
+	public void setResult(int result) {
+		this.result = result;
+	}
+	
 		
 		
 		
---
+++ Calculator/src/ies/badia/calculator/main/Subtraction.java
@@ -0,0 +1,21 @@
+/**
+ * Clase Estatica que recibe dos Strings como parametro
+ * Las convierte a enteros y los resta
+ */
+package ies.badia.calculator.main;
+
+/**
+ * @author Francisco José Camacho Maya
+ * @author Izar Mula soria
+ *
+ */
+public class Subtraction {
+	public static int result(String op1,String op2) {
+		int o1=Integer.parseInt(op1);
+		int o2=Integer.parseInt(op2);
+		
+		return o1-o2;
+		
+	}
+
+}
