37010a6bf08b81544ba3c969e4a67555b809a3db
Triburch <eeR_muuLa_97@hotmail.com>
2018-05-02 13:33:13 +0200

    Refactor module Product para seguir el modelo de desarrollo

--- Calculator/src/ies/badia/calculator/main/Product.java
+++ Calculator/src/ies/badia/calculator/main/Product.java
@@ -16,8 +16,14 @@ public class Product {
 	public static int result(String op1,String op2) {
 		int o1=Integer.parseInt(op1);
 		int o2=Integer.parseInt(op2);
+		int result=0;
 		
-		return o1*o2;
+		for(int i=0;i<o2;i++) {
+			result+=o1;
+			
+		}
+		
+		return result;
 		
 	}
 
