6a6e7c67c1e7d6037fb38b079bdca5d46430b95e
fenix15100 <francisco.camacho15@gmail.com>
2018-05-02 10:35:37 +0200

    Initial Commit
    Creacion del proyecto y su estructura.

---
+++ Calculator/src/ies/badia/calculator/main/Calculator.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package ies.badia.calculator.main;
+
+/**
+ * @author Francisco José Camacho Maya
+ * @author Izar Mula soria
+ *
+ */
+public class Calculator {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+
+	}
+
+}
