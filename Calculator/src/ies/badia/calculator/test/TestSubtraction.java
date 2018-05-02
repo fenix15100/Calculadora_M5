b6258e4b4cfaf05080f1cac755188d8233f0990b
fenix15100 <francisco.camacho15@gmail.com>
2018-05-02 13:07:15 +0200

    add one test to TestSubtraction
    He añadido un test que implique la resta negativa

--- Calculator/src/ies/badia/calculator/test/TestSubtraction.java
+++ Calculator/src/ies/badia/calculator/test/TestSubtraction.java
@@ -29,12 +29,23 @@ public int expected;
 	 * Test method for {@link ies.badia.calculator.main.Subtraction#result(java.lang.String, java.lang.String)}.
 	 */
 	@Test
-	public void testResult() {
+	public void restapositiva() {
 		String op1="7";
 		String op2="3";
 		
 		result=Subtraction.result(op1, op2);
 		expected=4;
+		assertEquals(expected, result);
+		
+	}
+	
+	@Test
+	public void restanegativa() {
+		String op1="3";
+		String op2="7";
+		
+		result=Subtraction.result(op1, op2);
+		expected=-4;
 		assertEquals(expected, result);
 		
 	}
